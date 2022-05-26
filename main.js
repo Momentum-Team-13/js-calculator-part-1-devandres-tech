7c1728a66b661ef7c3d4a4e9f14f57128dcd9ee2
andres alcocer <devandres.tech@gmail.com>
2022-05-26 10:39:55 -0400

    implemented event delegation to trigger a single click event

--- main.js
+++ main.js
@@ -2,22 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
   let userInputs = ''
   const display = document.getElementById('display')
 
-  document.querySelectorAll('.button').forEach((item) => {
-    item.addEventListener('click', (e) => {
-      display.textContent += e.target.innerHTML.replace(/x/g, '*')
-      userInputs += e.target.innerHTML
-    })
-  })
+  document.getElementById('wrapper').addEventListener('click', (e) => {
+    const inputEvent = e.target.innerHTML
+    display.textContent += e.target.innerHTML.replace(/x/g, '*')
 
-  document.getElementById('equals').addEventListener('click', () => {
-    const calculation = userInputs.replace(/x/g, '*')
-    const result = eval(calculation)
-    display.textContent = result
-    userInputs = ''
-  })
-
-  document.getElementById('clear').addEventListener('click', () => {
-    display.textContent = ''
-    userInputs = ''
+    if (inputEvent === '=') {
+      const calculation = userInputs.replace(/x/g, '*')
+      console.log('hiii', calculation)
+      const result = eval(calculation)
+      display.textContent = result
+      userInputs = ''
+    } else if (inputEvent === 'C') {
+      display.textContent = ''
+      userInputs = ''
+    } else {
+      userInputs += inputEvent
+    }
   })
 })
